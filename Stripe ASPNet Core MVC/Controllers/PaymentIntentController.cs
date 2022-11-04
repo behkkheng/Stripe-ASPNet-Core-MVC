93bce7d6339c739b522b7031a9a5b752f94e6ace
behkkheng <behkkheng@gmail.com>
2022-11-05 00:45:47 +0800

    Add project files.

---
+++ Stripe ASPNet Core MVC/Controllers/PaymentIntentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace Stripe_ASPNet_Core_MVC.Controllers
+{
+    public class PaymentIntentApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Set your secret key. Remember to switch to your live secret key in production.
+            // See your keys here: https://dashboard.stripe.com/apikeys
+            StripeConfiguration.ApiKey = "YOUR_SECRET_KEY";
+
+            //invoice bill
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = 1099,
+                Currency = "myr",
+                AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
+                {
+                    Enabled = true,
+                },
+            };
+            var service = new PaymentIntentService();
+            var intent = service.Create(options);
+
+            ViewData["ClientSecret"] = intent.ClientSecret;
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
